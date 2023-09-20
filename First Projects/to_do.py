904ebe91d853efa3125fd715ff16f5a0092454eb
Fabio <fabioperrone95@hotmail.com>
2023-09-19 22:57:57 -0300

    Versión funcional, con checkbox y textfield para anotar el to do

--- First Projects/to_do.py
+++ First Projects/to_do.py
@@ -1,7 +1,16 @@
 import flet as ft
 
 def main(page: ft.Page):
-    """Agrega a la página como texto el 'Hello World!'"""
-    page.add(ft.Text(value="Hello, World!"))
+    
+    new_task = ft.TextField(hint_text="Whats need to be done?")
+    
+    def add_clcked(e):
+        """Agrega a la página un checkbox con el label del valor de 'new_task'
+        y actualiza la página"""
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        page.update()        
+    # Agrega a la página, la nueva tarea y un botón de acción flotante y declaramos que al clickear se ejecute la función
+    page.add(new_task, ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clcked))
 
 ft.app(target= main)
