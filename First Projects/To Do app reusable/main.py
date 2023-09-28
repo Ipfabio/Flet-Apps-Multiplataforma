a826bca181db301d29d68aee222857a49f25850a
Fabio <fabioperrone95@hotmail.com>
2023-09-28 01:12:15 -0300

    Creo el main donde ejecuta la app ToDo

---
+++ First Projects/To Do app reusable/main.py
@@ -0,0 +1,23 @@
+import flet as ft
+from to_do import TodoApp
+
+
+def main(page: ft.Page):
+    """
+    Configura y ejecuta una aplicación de lista de tareas (ToDo).
+    """
+    page.title = "ToDo App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    # Create application instance
+    todo = TodoApp()
+    # app1 = TodoApp()
+    # app2 = TodoApp()
+
+    # add application's root control to the page
+    # page.add(app1, app2)
+    page.add(todo)
+
+
+ft.app(target=main)
