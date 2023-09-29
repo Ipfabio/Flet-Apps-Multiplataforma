a478543d23d360cc9b2376abf40a6abcc4cc5c81
Fabio <fabioperrone95@hotmail.com>
2023-09-29 01:28:24 -0300

    Agrego botones de filtro y funciones de actualizar

--- First Projects/To Do app reusable/to_do.py
+++ First Projects/To Do app reusable/to_do.py
@@ -13,6 +13,13 @@ class TodoApp(ft.UserControl):
         """
         self.new_task = ft.TextField(hint_text="Que hay que hacer?", expand=True)
         self.tasks = ft.Column()
+        
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change= self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed"),]
+        )
+        
 
         # Applcations's root control (i.e. "view") containing all other controls
         return ft.Column(
@@ -26,7 +33,13 @@ class TodoApp(ft.UserControl):
                         ),
                     ],
                 ),
-                self.tasks,
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ]
+                )
             ],
         )
 
@@ -37,6 +50,19 @@ class TodoApp(ft.UserControl):
         task = Task(self.new_task.value, self.task_delete)
         self.tasks.controls.append(task)
         self.new_task.value = ""
+        self.update()
+        
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+        super().update()
+    
+    def tabs_changed(self, e):
         self.update()
 
     def task_delete(self, task):
