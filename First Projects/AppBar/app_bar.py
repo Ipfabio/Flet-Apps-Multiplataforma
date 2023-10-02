1711d97e97a429bef8f25861348d278355e73665
Fabio <fabioperrone95@hotmail.com>
2023-10-02 13:36:53 -0300

    Agrego comentarios

--- First Projects/AppBar/app_bar.py
+++ First Projects/AppBar/app_bar.py
@@ -2,10 +2,26 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    """
+    Configura y ejecuta una aplicación de ejemplo con una AppBar personalizada y controles interactivos.
+
+    Args:
+        page (ft.Page): La página principal de la aplicación.
+
+    """
     def check_item_clicked(e):
+        """
+        Maneja el evento de clic en un elemento de menú emergente.
+        Cambia el estado de marcado o desmarcado del elemento y actualiza la página.
+
+        Args:
+            e (ft.Event): El evento de clic.
+
+        """
         e.control.checked = not e.control.checked
         page.update()
 
+    # Configuración de la AppBar personalizada
     page.appbar = ft.AppBar(
         leading=ft.Icon(ft.icons.PALETTE),
         leading_width=40,
@@ -18,7 +34,7 @@ def main(page: ft.Page):
             ft.PopupMenuButton(
                 items=[
                     ft.PopupMenuItem(text="Item 1"),
-                    ft.PopupMenuItem(),  # divider
+                    ft.PopupMenuItem(),  # Divider
                     ft.PopupMenuItem(
                         text="Checked item", checked=False, on_click=check_item_clicked
                     ),
@@ -26,7 +42,8 @@ def main(page: ft.Page):
             ),
         ],
     )
+    # Agrega un control de texto al cuerpo de la página
     page.add(ft.Text("Body!"))
 
-
+# Inicia la aplicación
 ft.app(target=main)
